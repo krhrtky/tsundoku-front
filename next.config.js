4f9d7baf61baf85ceb5090cc6c1f922223469ed6
t-kurihara <takuya.kurihara00@gmail.com>
2020-03-22 19:44:17 +0900

    [fix] reset css.

--- next.config.js
+++ next.config.js
@@ -2,11 +2,15 @@
 const path = require('path');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withSourceMaps = require('@zeit/next-source-maps');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withSass = require('@zeit/next-sass');
 
-module.exports = withSourceMaps({
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  webpack: config => {
-    config.resolve.alias['@'] = path.resolve(`${__dirname}/src`);
-    return config;
-  }
-});
+module.exports = withSass(
+  withSourceMaps({
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    webpack: config => {
+      config.resolve.alias['@'] = path.resolve(`${__dirname}/src`);
+      return config;
+    }
+  })
+);
