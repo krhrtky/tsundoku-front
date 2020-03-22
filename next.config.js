e2c0b1dce9684f10db1dc5a112c786be581d43ee
t-kurihara <takuya.kurihara00@gmail.com>
2020-03-22 22:29:46 +0900

    [add] build settings.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withPlugins } = require('next-compose-plugins');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withSourceMaps = require('@zeit/next-source-maps');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withSass = require('@zeit/next-sass');
 
-module.exports = withSass(
-  withSourceMaps({
-    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    webpack: config => {
-      config.resolve.alias['@'] = path.resolve(`${__dirname}/src`);
-      return config;
-    }
-  })
-);
+const nextConfig = {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  webpack: config => {
+    config.resolve.alias['@'] = path.resolve(`${__dirname}/src`);
+    return config;
+  }
+};
+
+module.exports = withPlugins([[withSass], [withSourceMaps]], nextConfig);
