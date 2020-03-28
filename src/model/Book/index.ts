3d80be98efc8f5a5b9419821b9f6ebd637e053ab
t-kurihara <takuya.kurihara00@gmail.com>
2020-03-28 22:03:41 +0900

    [Refactor] Book type definitions.

--- src/model/Book/index.ts
+++ src/model/Book/index.ts
@@ -1,5 +1,6 @@
 export { Book } from './Book';
 export { Id, Name, Link } from './vo';
+export { Types } from './vo';
 import { Type as T, Status as S } from './vo';
 export type Type = T;
 export type Status = S;
